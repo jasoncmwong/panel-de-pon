404accd52f49fadc0010bbd24970876473916b3b
jcwong2 <jcwong2@ualberta.ca>
2020-06-06 20:25:55 -0600

    Modified variable naming convention to underscore.

--- src/states/StartState.lua
+++ src/states/StartState.lua
@@ -1,12 +1,22 @@
 StartState = Class{__includes = BaseState}
 
 
+function StartState:update()
+    if love.keyboard.was_pressed('escape') then
+        love.event.quit()
+    end
+
+    if love.keyboard.was_pressed('enter') or love.keyboard.was_pressed('return') then
+        g_state_machine:change('play')
+    end
+end
+
 function StartState:render()
-    local rectWidth = 200
-    local rectHeight = 50
+    local rect_width = 200
+    local rect_height = 50
     love.graphics.setColor(111/255, 203/255, 159/255, 255/255)
-    love.graphics.rectangle('fill', (VIRTUAL_WIDTH-rectWidth)/2, (VIRTUAL_HEIGHT-rectHeight)/2, rectWidth, rectHeight)
+    love.graphics.rectangle('fill', (VIRTUAL_WIDTH-rect_width)/2, (VIRTUAL_HEIGHT-rect_height)/2, rect_width, rect_height)
     love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.setFont(gFonts['large'])
-    love.graphics.printf("PANEL DE PON", (VIRTUAL_WIDTH-rectWidth)/2, (VIRTUAL_HEIGHT-gFonts['large']:getHeight())/2, rectWidth, 'center')
+    love.graphics.setFont(g_fonts['large'])
+    love.graphics.printf("PANEL DE PON", (VIRTUAL_WIDTH-rect_width)/2, (VIRTUAL_HEIGHT-g_fonts['large']:getHeight())/2, rect_width, 'center')
 end
