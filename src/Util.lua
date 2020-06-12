4c598d7fdd090ed10052af9ce116d811888abbca
jcwong2 <jcwong2@ualberta.ca>
2020-06-11 18:48:44 -0600

    Added extra table utility functions (slice, extend or concatenate, and full).

--- src/Util.lua
+++ src/Util.lua
@@ -16,3 +16,41 @@ end
 function gen_cursor_quad(atlas)
     return love.graphics.newQuad(0, 0, 18, 18, atlas:getDimensions())
 end
+
+--[[
+    Slices tables like Python
+]]
+function table.slice(tbl, first, last, step)
+    local slice = {}
+  
+    for i = first or 1, last or #tbl, step or 1 do
+      slice[#slice+1] = tbl[i]
+    end
+    return slice
+end
+
+--[[
+    Concatenates or extends two tables
+]]
+function table.extend(tbl1, tbl2)
+    if #tbl2 ~= 0 then
+        for i = 1, #tbl2 do
+            tbl1[#tbl1 + 1] = tbl2[i]
+        end
+    end
+    return tbl1
+end
+
+--[[
+    Creates a 2D table with each index initialized with the specified value
+]]
+function table.full(rows, cols, val)
+    full = {}
+    for i = 1, rows do
+        table.insert(full, {})
+        for j = 1, cols do
+            table.insert(full, {val})
+        end
+    end
+    return full
+end
